2bae0be90f4c0809a834d13066cd6260b0717f57
FA20-BSE-045 <faizanzaheer531@gmail.com>
2024-09-30 08:51:21 -0700

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+    <nav>
+    <ul>
+            <li><a href="Google.com">Home</a></li>
+            <li><a href="Google.com">Landing</a></li>
+            <li><a href="Google.com">About</a></li>
+            <li><a href="Google.com">Team</a></li>
+            <li><a href="Google.com">Gallery</a></li>
+            <li><a href="Google.com">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+  );
+};
+
+export default Header;
---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import './Menu.css';
+
+const CampPage = () => {
+    return (
+        <>
+
+        <div className="hero">
+            <div className='shape'>
+                <h1>40%</h1>
+                <p>Business <br /> Lunch</p>
+            </div>
+                <div className="content">
+                    <h1>Where every ingredient <br /> tells a story</h1>
+                    <button>READ MORE</button>
+                        </div>
+                        </div>
+
+<div>
+            <div className="Experience">
+            <h4>View Our Menu  </h4>
+          </div>
+
+          <div className="menu-cards">
+          <div className="card">
+            <img src="https://assets.nicepagecdn.com/11a8ddce/4267640/images/delicious-pizza-isolated-white.jpg" alt="hot pizza"/>
+            <h3>HOT PIZZA</h3>
+            <button>READ MORE</button>
+          </div>
+      
+          <div className="card">
+            <img src="https://assets.nicepagecdn.com/11a8ddce/4267640/images/high-angle-shot-vegetable-salad-white-bowl.jpg" alt="salads"/>
+            <h3>SALADS</h3>
+            <button>READ MORE</button>
+          </div>
+      
+          <div className="card">
+            <img src="https://assets.nicepagecdn.com/11a8ddce/4267640/images/pexels-photo-3681641.jpeg" alt="dessert"/>
+            <h3>DESSERT</h3>
+            <button>READ MORE</button>
+          </div>
+      
+          <div className="card">
+            <img src="https://assets.nicepagecdn.com/11a8ddce/4267640/images/pexels-photo-3939602.jpeg" alt="drinkss"/>
+            <h3>DRINKS</h3>
+            <button>READ MORE</button>
+          </div>
+        </div>
+</div>
+
+<div className="contact-container">
+      <div className="contact-card">
+        <h1>Keep up to date with us</h1>
+        <p>Sample text. Click to select the Text Element.</p>
+        <div className="contact-info">
+          <span role="img" aria-label="phone">📞</span> 
+          <strong>+1 (234) 567-8910</strong>
+        </div>
+      </div>
+    </div>
+
+    <div className="second-section">
+                <div className="section">
+                    <h1>A culinary  <br />adventure for all <br /> the senses</h1>
+                    <button>READ MORE</button>
+                        </div>
+                        </div>
+
+
+
+<div className="four-section">
+            <h4>Check Our Blog</h4>
+          </div>  
+  <div className="section-cards">
+          <div className="card">
+            <img src="https://assets.nicepagecdn.com/11a8ddce/4267640/images/pexels-photo-3662126.jpeg" alt="Post 6 Headline"/>
+            <h3>Post 6 Headline</h3>
+            <button>READ MORE</button>
+          </div>
+      
+          <div className="card">
+            <img src="https://assets.nicepagecdn.com/11a8ddce/4267640/images/pexels-photo-3906526.jpeg" alt="Post 5 Headline"/>
+            <h3>Post 5 Headline</h3>
+            <button>READ MORE</button>
+          </div>
+      
+          <div className="card">
+            <img src="https://assets.nicepagecdn.com/11a8ddce/4267640/images/pexels-photo-3850972.jpeg" alt="Post 4 Headline"/>
+            <h3>Post 4 Headline</h3>
+            <button>READ MORE</button>
+          </div>
+      
+          <div className="card">
+            <img src="https://assets.nicepagecdn.com/11a8ddce/4267640/images/pexels-photo-3939685.jpeg" alt="Post 3 Headline"/>
+            <h3>Post 3 Headline</h3>
+            <button>READ MORE</button>
+          </div>
+        </div>
+
+        <div className="five-section">
+                <div className="five">
+                    <h1>The magic of <br /> the kitchen</h1>
+                    <button>READ MORE</button>
+                        </div>
+                        </div>
+
+
+<div className="container">
+      <div className="contact-card">
+        <h1>Keep up to date with us</h1>
+        <p>Sample text. Click to select the Text Element.</p>
+        <div className="contact-info">
+          <span role="img" aria-label="phone">📞</span> 
+          <strong>+1 (234) 567-8910</strong>
+        </div>
+      </div>
+    </div>
+                   </>
+                        );
+                        };
+
+                        export default CampPage;
