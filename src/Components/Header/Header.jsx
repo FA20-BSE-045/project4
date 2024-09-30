cb98accaff367bff83a221ca342ac028f802ca68
FA20-BSE-045 <faizanzaheer531@gmail.com>
2024-09-30 08:59:52 -0700

    first commit

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,8 +8,6 @@ const Header = () => {
             <li><a href="Google.com">Home</a></li>
             <li><a href="Google.com">Landing</a></li>
             <li><a href="Google.com">About</a></li>
-            <li><a href="Google.com">Team</a></li>
-            <li><a href="Google.com">Gallery</a></li>
             <li><a href="Google.com">Contact</a></li>
         </ul>
     </nav>
